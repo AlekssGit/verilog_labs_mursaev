337bdbfdc1ae475190ec804e2491ee7d00fb5155
AlekssGit <alvasin98@gmail.com>
2021-05-17 13:00:36 +0300

    add maketirovanie

--- Lab_3 Quartus/Tb__l3.sv
+++ Lab_3 Quartus/Tb__l3.sv
@@ -1,14 +1,15 @@
-module Tb__l3(data_out, i_clock, lcd, digits);
+module Tb__l3(leds_data_out, i_clock, lcd, digits);
 	input logic i_clock;				// input clock
 	output logic [6:0] lcd;			// lcd semisegment pins
 	output logic [3:0] digits;		//control semisegment digits
-	output logic [2:0] data_out; //connect to the leds
-	
+	output logic [2:0] leds_data_out; //connect to the leds
+	logic [2:0] data_out;
 	logic [2:0] data_in;
 	
 	logic clock;
-	logic lcd_inv;
+	logic [6:0] lcd_inv;
 	assign lcd = ~lcd_inv;
+	assign leds_data_out = ~data_out;
 	
 	//Turn on digigtal 1
 	assign digits[0] = 1'b0;
@@ -18,7 +19,7 @@ module Tb__l3(data_out, i_clock, lcd, digits);
 	
 	generator  #(25000000) gen (i_clock, clock); 			//For working with board: 25000000; 
 	
-	semisegment sem (.number(data_in), .result(lcd_inv));	//lcd_inv
+	semisegment sem (.number({1'b0, data_in}), .result(lcd_inv));	//lcd_inv
 
 	counter cnt (.clock(clock), .q(data_in));
 	complex m2 (.data_in(data_in), .data_out(data_out)); 
--- Lab_5 Quartus/struct_l6.sv
+++ Lab_5 Quartus/struct_l6.sv
@@ -1,35 +1,38 @@
-module struct_l6(clock, reset, start, ready, in_data, result);
+module struct_l6(clock, i_reset, i_start, o_ready, in_data, lcd, digits);
 
 parameter data_size = 8;
 
 //input logic i_clock;				// input clock
-//output logic [6:0] lcd;			// lcd semisegment pins
-//output logic [3:0] digits;		//control semisegment digits
+output logic [6:0] lcd;			// lcd semisegment pins
+output logic [3:0] digits;		//control semisegment digits
 
 
 input logic clock;
-input logic start;
-input logic reset;
+input logic i_start;
+input logic i_reset;
 input logic [data_size - 1 : 0] in_data;
-output logic ready;
-output logic [data_size - 1 : 0] result;
+output logic o_ready;
+logic [data_size - 1 : 0] result;
 
-//logic clock;
-//logic lcd_inv;
-//assign lcd = ~lcd_inv;
-
-////Turn on digigtal 1
-//assign digits[0] = 1'b0;
-//assign digits[1] = 1'b1;
-//assign digits[2] = 1'b1;
-//assign digits[3] = 1'b1;
-
-//generator  #(25000000) gen (i_clock, clock); 			//For working with board: 25000000; 
-
-//semisegment sem (.number(result[3:0]), .result(lcd_inv));	//lcd_inv
-
+logic start;
+assign start = ~i_start;
+logic reset;
+assign reset = ~i_reset;
+logic ready;
+assign o_ready = ~ready;
 
 mp_mult #(8) mult (in_data, clock, reset, start, result, ready);
+
+
+logic [3:0] ready_result;
+
+
+always@ (posedge ready)
+begin
+	ready_result = result[3:0];
+end
+
+lcd lcd_show (.i_clock(clock), .data_dig_0(in_data[7:4]), .data_dig_1(in_data[3:0]), .data_dig_2(4'h0), .data_dig_3(ready_result), .digits(digits), .lcd_out(lcd));
 
 endmodule
 
@@ -55,4 +58,73 @@ shift_register #(data_size,1,"right") shift_mod(.clk(clock),.s(contr_sh_reg),.dr
   
 control_unit #(data_size) control_mod (.clock(clock), .reset(reset), .start(start), .y(yp[0]), .c_sh_rg(contr_sh_reg), .c_cnt(contr_cnt), .ready(ready));
 
+endmodule
+
+module lcd (i_clock, data_dig_0, data_dig_1, data_dig_2, data_dig_3, digits, lcd_out);
+
+input logic i_clock;
+input logic [3:0] data_dig_0;
+input logic [3:0] data_dig_1;
+input logic [3:0] data_dig_2;
+input logic [3:0] data_dig_3;
+output logic [3:0] digits;
+output logic [6:0] lcd_out;
+
+
+reg [6:0] led_inv_dig_0;
+reg [6:0] led_inv_dig_1;
+reg [6:0] led_inv_dig_2;
+reg [6:0] led_inv_dig_3;
+
+assign led_inv_dig_2 = 7'b1000000; //symbol "-"
+
+//Понижение частоты для переключения цифр, так как сегменты объединены в одну цепь
+reg clk_led;
+generator  #(100000) gen_led (i_clock, clk_led);
+
+semisegment seg_dig_1 (.number(data_dig_1), .result(led_inv_dig_1));
+semisegment seg_dig_0 (.number(data_dig_0), .result(led_inv_dig_0));
+
+semisegment seg_dig_3 (.number(data_dig_3), .result(led_inv_dig_3));
+//semisegment seg_dig_2 (.number(3'h0), .result(led_inv_dig_2));
+
+always@(posedge clk_led)
+begin
+	if(digits[0] == 1'b0)
+	begin
+		digits[0] = 1'b1;
+		digits[1] = 1'b0;
+		digits[2] = 1'b1;
+		digits[3] = 1'b1;
+		
+		lcd_out = ~led_inv_dig_1;
+	end
+	else if(digits[1] == 1'b0)
+	begin
+		digits[0] = 1'b1;
+		digits[1] = 1'b1;
+		digits[2] = 1'b0;
+		digits[3] = 1'b1;
+		
+		lcd_out = ~led_inv_dig_2;
+	end
+	else if(digits[2] == 1'b0)
+	begin
+		digits[0] = 1'b1;
+		digits[1] = 1'b1;
+		digits[2] = 1'b1;
+		digits[3] = 1'b0;
+		
+		lcd_out = ~led_inv_dig_3;
+	end
+	else
+	begin
+		digits[0] = 1'b0;
+		digits[1] = 1'b1;
+		digits[2] = 1'b1;
+		digits[3] = 1'b1;
+		
+		lcd_out = ~led_inv_dig_0;
+	end
+end
 endmodule
